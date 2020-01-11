fe71ae6ab77c2fd8825d4ee26d6356f0b6f060cd
Marcelo Pio de Castro <marcelopiocastro@gmail.com>
2020-01-11 15:17:12 -0300

    Arquivos gerados pelo Spring Initializr
    Commit contendo os arquivos iniciais gerados pelo Spring Initializr

---
+++ src/test/java/br/com/belfalas/restfileupload/RestFileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.belfalas.restfileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFileUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
